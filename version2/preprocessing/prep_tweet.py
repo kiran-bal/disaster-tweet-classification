0bd18ad1afa70523edcaa95e2530e3af668d3597
k4kiran <kforkiran005@gmail.com>
2020-02-05 17:56:18 +0530

    version 2 started

---
+++ version2/preprocessing/prep_tweet.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+df = pd.read_csv('../dataset/train.csv')
+#print(df)
+
+train_df = df[['text','target']]
+
+# Cleaning punctuations n special characters
+import re
+
+puncts = [',', '.', '"', ':', ')', '(', '-', '!', '?', '|', ';', "'", '$', '&', '/', '[', ']', '>', '%', '=', '*', '+', '\\', '•',  '~', '£', 
+ '·', '_', '{', '}', '©', '^', '®', '`',  '<', '→', '°', '€', '™', '›',  '♥', '←', '×', '§', '″', '′', 'Â', '█', '½', 'à', '…', 
+ '“', '★', '”', '–', '●', 'â', '►', '−', '¢', '²', '¬', '░', '¶', '↑', '±', '¿', '▾', '═', '¦', '║', '―', '¥', '▓', '—', '‹', '─', 
+ '▒', '：', '¼', '⊕', '▼', '▪', '†', '■', '’', '▀', '¨', '▄', '♫', '☆', 'é', '¯', '♦', '¤', '▲', 'è', '¸', '¾', 'Ã', '⋅', '‘', '∞', 
+ '∙', '）', '↓', '、', '│', '（', '»', '，', '♪', '╩', '╚', '³', '・', '╦', '╣', '╔', '╗', '▬', '❤', 'ï', 'Ø', '¹', '≤', '‡', '√', ]
+
+def clean_text(x):
+	x = str(x)
+	for punct in puncts:
+		if punct in x:
+			x = x.replace(punct, f' ')
+	return x
+
+print(train_df)
+for index_label,row_series in df.iterrows():
+	train_df.at[index_label , 'text'] = clean_text(row_series['text'])
+print(train_df)
